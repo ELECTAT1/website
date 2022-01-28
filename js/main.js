5b65ea9607ff8cebfd116d060838c09f7dbedbe5
ELECTAT1 <98615781+ELECTAT1@users.noreply.github.com>
2022-01-29 01:10:17 +0530

    ok

---
+++ js/main.js
@@ -0,0 +1,58 @@
+
+// services  carousel start
+$('.services-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    autoplay:true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+        },
+        600:{
+            items:2,  
+        },
+        1000:{
+            items:3,  
+        }
+    }
+});
+// services carousel start
+// testimonial carousel start
+$('.testimonial-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    autoplay:true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+        },
+        600:{
+            items:2,  
+        },
+        1000:{
+            items:3,  
+        }
+    }
+});
+// testimonial carousel end
+// team carousel start
+$('.team-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    autoplay:true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+        },
+        600:{
+            items:2,  
+        },
+        1000:{
+            items:3,  
+        }
+    }
+});
+// team carousel end
